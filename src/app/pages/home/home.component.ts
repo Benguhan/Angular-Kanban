230abfd059426bedec70a83ea495c8028dc94a65
Bengühan Şahin <benguhansahin@gmail.com>
2024-07-16 11:08:33 +0300

    add task columns to the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,17 +3,69 @@ import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { PickListModule } from 'primeng/picklist';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink,CdkDropListGroup, CdkDropList, CdkDrag, PickListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   authService = inject(AuthService);
-  date: Date | undefined;
+
+
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  review = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  inProgress = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
+
+  // allowDrop(event: DragEvent): void {
+  //   event.preventDefault();
+  // }
+
+  // drag(event: DragEvent): void {
+  //   if (event.dataTransfer) {
+  //     event.dataTransfer.setData('text', (event.target as HTMLElement).id);
+  //   }
+  // }
+
+  // drop(event: DragEvent): void {
+  //   event.preventDefault();
+  //   const data = event.dataTransfer?.getData('text');
+  //   if (data) {
+  //     const element = document.getElementById(data);
+  //     if (element) {
+  //       (event.target as HTMLElement).appendChild(element);
+  //     }
+  //   }
+  // }
 
   ngOnInit(): void {
     this.authService.user$.subscribe(user => {
